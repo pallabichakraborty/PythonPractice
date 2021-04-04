c85168578fff540b9ec2041a45fb61caf8148c40
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-04 13:23:59 -0500

    Update TurtleGraphics.py
    Added pencolor to the program

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -7,6 +7,7 @@ sys.setExecutionLimit(200000)
 
 turtleName = ['Liam','Noah','Oliver','William','Elijah','James','Benjamin','Lucas','Mason','Ethan','Olivia','Emma','Ava','Sophia','Isabella','Charlotte','Amelia','Mia','Harper','Evelyn']
 turtleGroup=[]
+colordata=['red','blue','green','orange','yellow','brown','pink','gray','turquoise','violet']
 
 distance = 50
 angle=10
@@ -15,7 +16,8 @@ for i in range(len(turtleName)):
     turtleGroup.append(turtle.Turtle())
     turtleGroup[i].name=turtleName[i]
     turtleGroup[i].pensize(i)
-    turtleGroup[i].pencolor('red')
+    # Add color to the arrows
+    turtleGroup[i].pencolor(colordata[i%10])
     turtleGroup[i].right(angle)
     turtleGroup[i].forward(distance)
     turtleGroup[i].write(turtleGroup[i].name)
