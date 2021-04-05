58b47cd777bae763520d803bd10efae95a5a5047
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-05 14:56:43 -0500

    Changed angle and distance
    Changed angle and distance

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -15,14 +15,13 @@ angle=10
 for i in range(len(turtleName)):
     turtleGroup.append(turtle.Turtle())
     turtleGroup[i].name=turtleName[i]
-    turtleGroup[i].pensize(i)
+    turtleGroup[i].pensize(i%10)
     # Add color to the arrows
     turtleGroup[i].pencolor(colordata[i%10])
     turtleGroup[i].right(angle)
     turtleGroup[i].forward(distance)
     turtleGroup[i].write(turtleGroup[i].name)
-    angle=angle+20
-    distance=distance+30
+    angle=angle+18
+    distance=distance+10
     
   
-    
